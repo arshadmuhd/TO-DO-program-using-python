15ca302e85a8d4b257dfd8e01b062bdf2863f12b
arshadmuhd <arshadmuhd2001@gmail.com>
2021-06-21 21:13:46 +0530

    initialize

---
+++ todo.py
@@ -0,0 +1,12 @@
+# simple python to-do app project
+user_input = 'random'
+# crete a list for items
+data = list()
+
+
+# we are going to create 4 options
+# 1.Add an item
+# 2.Mark as done
+# 3.View items
+# 4.exit
+
