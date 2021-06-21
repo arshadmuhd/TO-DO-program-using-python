2ed713a1dd39195917b727ad084745be3997a363
arshadmuhd <arshadmuhd2001@gmail.com>
2021-06-21 21:16:10 +0530

    to-do functions

--- todo.py
+++ todo.py
@@ -20,3 +20,28 @@ def displayMenu():
     print("4.exit:")
 
 
+while user_input != '4':
+
+    displayMenu()
+    user_input = input("Enter your choice:")
+
+    if user_input == '1':
+        item = input("what is to be done?")
+        data.append(item)
+        print("added items are:" + item)
+    elif user_input == '2':
+        item = input("what is to be marked as done")
+        # if item is present in data list then remove it from the list
+        # else print does not exist in list
+        if item in data:
+            data.remove(item)
+            print("removed items:" + item)
+        else:
+            print("item does not exist in the to-do list")
+    elif user_input == '3':
+        print("list of TO-DO items:")
+        for items in data:
+            print(item)
+
+    elif user_input == '4':
+        print("good bye")
